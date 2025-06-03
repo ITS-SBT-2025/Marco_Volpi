558f8e0c678f58a8e9d71cdf3adcdfed65207709
LacaAzzone <volpi.marco95@gmail.com>
2025-06-03 14:28:35 +0200

    Add initial version of aggiornamento file

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+
+console.log('Hello, world!');
+
+
+const http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, 
+  { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+  res.end
+  }).listen(8080);
